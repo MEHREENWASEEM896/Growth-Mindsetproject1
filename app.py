71946c9656ff2cdf95924403e074582b17923655
Mehreen Waseem <mehreenidrees192@gmail.com>
2025-02-28 21:39:19 +0500

    app.py

--- app.py
+++ app.py
@@ -83,11 +83,11 @@ if st.checkbox(f"clean data for{file.name}"):
             mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         buffer.seek(0)
       #Download Button
-     st.download_button(
-         label=f"Download {file.name} as {conversion_type}",
-         data=buffer,
-          file_name=file_name,
-          mime=mime_type
+         st.download_button(
+            label=f"Download {file.name} as {conversion_type}",
+            data=buffer,
+            file_name=file_name,
+            mime=mime_type
         )
 
     st.success("All files processed successfully!")
